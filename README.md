e6eaae037d746ff10f2522a1985dfcc19362965d
Danny Smith <danny.j.smith@oracle.com>
2017-11-23 12:55:14 +0000

    Creating moustache-api by copying beard-api.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Moustache API
+
+A silly API that will be part of a Kubernetes demo.
+
+## Prerequisites
+
+You will need [Leiningen][] 2.0.0 or above installed.
+
+[leiningen]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## License
+
+Copyright © 2017 FIXME
---
+++ src/moustache_api/handler.clj
@@ -0,0 +1,74 @@
+(ns moustache-api.handler
+  (:gen-class)
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.json :as json]
+            [config.core :as config.core]
+            [clojure.string :as string])
+  (:import [java.net InetAddress UnknownHostException]))
+
+(defonce env
+  (merge
+   {:port "5001"}
+   config.core/env))
+
+(def moustache-styles
+  [{:name "Chevron"}
+   {:name "English"}
+   {:name "Handlebar"}
+   {:name "Horseshow"}
+   {:name "Pencil"}
+   {:name "Walrus"}])
+
+(defn get-ip-address
+  []
+  (str (InetAddress/getLocalHost)))
+
+(defn- add-to-host-sequence
+  [host-sequence]
+  (string/join " "
+            (cons (get-ip-address)
+                  (string/split (or host-sequence "") #" "))))
+
+(defn hostname-middleware
+  [handler]
+  (fn [request]
+    (let [{{host-sequence "X-Host-Sequence" :as headers} :headers :as response} (handler request)]
+      (assoc response :headers
+             (assoc headers "X-Host-Sequence" (add-to-host-sequence host-sequence))))))
+
+(defroutes app-routes
+  (GET "/" []
+    {:status 200
+     :body {:styles moustache-styles}})
+
+  (GET "/request" request
+      {:status 200
+       :body (select-keys request [:headers
+                                   :server-port
+                                   :server-name
+                                   :remote-addr
+                                   :uri
+                                   :query-string
+                                   :scheme
+                                   :request-method])})
+
+  (GET "/address" []
+    {:status 200
+     :body {:address (get-ip-address)}})
+
+  (route/not-found "Not Found"))
+
+(def app
+  (->
+   app-routes
+   (hostname-middleware)
+   (json/wrap-json-response)
+   (json/wrap-json-body {:keywords? true})
+   (wrap-defaults site-defaults)))
+
+
+(defn -main [& args]
+  (run-jetty #'app {:port (Integer/parseInt (:port env)) :join? true}))
